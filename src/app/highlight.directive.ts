636faafcfb7f2098151be401c39dec2c36630bee
Albert Omware <aomware@gmail.com>
2019-01-18 06:30:38 +0300

    added highlight directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, } from '@angular/core';
+import { Directive, ElementRef, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
@@ -7,6 +7,16 @@ import { Directive, } from '@angular/core';
 export class HighlightDirective {
 
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
+@HostListener('mouseenter') onMouseEnter() {
+  this.highlight('2AC7EB');
+}
 
+@HostListener('mouseleave') onMouseLeave() {
+  this.highlight(null);
+}
+
+private highlight(color: string) {
+  this.el.nativeElement.style.backgroundColor = color;
+}
 }

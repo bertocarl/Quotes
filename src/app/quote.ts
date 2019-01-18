0d5e0d3901aa3062d99859b4fb1819a5e6e8dda9
Albert Omware <aomware@gmail.com>
2019-01-18 19:11:50 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,6 +0,0 @@
-export class Quote {
-  public showDescription:boolean;
-    constructor(public id:number, public name:string, public author:string, public publisher:string, public upVote:number, public downVote:number, public completeDate:Date){
-    
-  }
-}
